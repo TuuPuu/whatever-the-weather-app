f49b42d8f753a7fab1d5da8c99cd7711fb5698eb
TuPu <141525093+TuuPuu@users.noreply.github.com>
2023-12-19 13:37:43 +0000

    added code to handle city search in form

--- src/weather.js
+++ src/weather.js
@@ -4,5 +4,15 @@
  2) FETCH FUNCTION COLLECT WEATHER DATA FROM API
  3) UPDATE PAGE WITH FETCHED DATA AND DISPLAY */
 
+// 1) CODE TO HANDLE CITY FORM SEARCH
+function handleFormSubmit() {
+  // Prevent the default form submission behavior
 
- 
+  const cityInput = document.getElementById("search-input");
+  console.log(cityInput);
+}
+
+const searchForm = document.getElementById("search-form");
+searchForm.addEventListener("submit", handleFormSubmit);
+
+handleFormSubmit();

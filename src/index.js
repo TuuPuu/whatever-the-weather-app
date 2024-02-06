a75727f12cabda54a0a7b9b5c1c16c03610fdb9f
TuPu <141525093+TuuPuu@users.noreply.github.com>
2024-02-06 23:25:38 +0000

    Added function for bettwer user experience. User input is trimmed and capitalised before being injected into h1 element

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 /*SEARCH FORM CODE*/
 
+// function to access user input and change city based on input
 function handleSearch(event) {
   event.preventDefault();
 
-  let searchInput = document.querySelector("#search-form-input");
-  let cityHeading = document.querySelector("#city-heading");
+  // function to capitalise accessed user input
+  function capitaliseLetter() {
+    let submittedUserInput = userInput.value;
+    const trimmedInput = submittedUserInput.trim();
+    const firstLetter = trimmedInput.charAt(0);
+    const firstLetterCap = firstLetter.toUpperCase();
+    const remainingLetters = trimmedInput.slice(1);
+    const updatedRemainingLetters = remainingLetters.toLowerCase();
+    const updatedTrimmedInput = firstLetterCap + updatedRemainingLetters;
 
-  cityHeading.innerHTML = searchInput.value;
+    return updatedTrimmedInput;
+  }
+
+  let userInput = document.querySelector("#search-form-input");
+  let cityHeading = document.querySelector("#city-heading");
+  let updatedInput = capitaliseLetter();
+
+  cityHeading.innerHTML = updatedInput;
 }
 
 let searchFormElement = document.querySelector("#search-form");
